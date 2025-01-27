2c7ab11140c63f5f04bfd1f6d6f05f0eb4106e60
Benjamin Grolleau <127044450+Grolleau-Benjamin@users.noreply.github.com>
2025-01-27 16:24:20 +0100

    fix(IFoo): Fix IFoo based on schema and not tests

--- src/test/java/com/jad/IFoo.java
+++ src/test/java/com/jad/IFoo.java
@@ -6,7 +6,5 @@ public interface IFoo {
 
     public void setCorge(ICorge corge);
 
-    // Only there because I need 3 functions and I don't understand which one is the last one
-    // to implement in this interface so fill it to avoid warning
-    public IQux getQux();
+    public void addBar(IBar bar);
 }
