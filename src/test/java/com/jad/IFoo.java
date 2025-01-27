f1cfa55cccf862a1a884635f3a11126246a7b441
Benjamin Grolleau <127044450+Grolleau-Benjamin@users.noreply.github.com>
2025-01-27 10:49:49 +0100

    fix: update tests

--- src/test/java/com/jad/IFoo.java
+++ src/test/java/com/jad/IFoo.java
@@ -1,6 +1,12 @@
 package com.jad;
 
 public interface IFoo {
+
     public ICorge getCorge();
+
     public void setCorge(ICorge corge);
+
+    // Only there because I need 3 functions and I don't understand which one is the last one
+    // to implement in this interface so fill it to avoid warning
+    public IQux getQux();
 }
